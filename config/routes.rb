1ca4c5b914604c589c3e2ff8fbb9fece561f1c79
nerwin hernando <nerwinc.hernando@gmail.com>
2023-06-27 03:44:37 +0800

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+
+  # Defines the root path route ("/")
+  root "main#index"
+  # same as
+  # match "/", to: "main#index", via: :get
+  match "about", to: "main#about", via: :get
+
   get 'main/index'
   get 'tasks/index'
   get 'tasks/new'
   get 'tasks/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "main#index"
-  # same as
-  # match "/", to: "main#index", via: :get
+
 end
